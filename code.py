f574919c81997701f9928f23044c5d17b745c545
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2023-01-11 18:37:32 +0000

    Finished cpt-03

--- code.py
+++ code.py
@@ -12,18 +12,31 @@ def game_scene():
     # This function is the main game scene
     
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    
+    # Sets the background to image 0 in Bank
+    # Sets tile size
     background = stage.Grid(image_bank_background, 10, 8)
     
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    
+    # Create stage for game
+    # Set frame rate to 60 per second
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     print("\n\n\n") # 3 blank lines
     print("Hello, World!")
 
     while True:
-        # Repeat forever, or until you turn it off!
-        pass # just a place holder
+        # Get user input
+        
+        # Update game logic
+        
+        # Redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
